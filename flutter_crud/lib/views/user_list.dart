e3165edc47fd89aae37aad7fb9b8f5e25c16b78d
Joao Ricardo Obara <joao.ricardo.obara@gmail.com>
2022-04-17 15:49:42 -0400

    provider 2

--- flutter_crud/lib/views/user_list.dart
+++ flutter_crud/lib/views/user_list.dart
@@ -1,25 +1,25 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/components/user_tile.dart';
-import 'package:flutter_application_1/data/dummy_users.dart';
+import 'package:flutter_application_1/provider/users.dart';
+import 'package:provider/provider.dart';
 
 class UserList extends StatelessWidget {
   const UserList({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    final users = {...DUMMY_USERS};
+    final Users users = Provider.of(context);
 
     return Scaffold(
       appBar: AppBar(
-        // ignore: prefer_const_constructors
-        title: Text('Lista de Usuários'),
+        title: const Text('Lista de Usuários'),
         actions: <Widget>[
           IconButton(onPressed: () {}, icon: const Icon(Icons.add))
         ],
       ),
       body: ListView.builder(
-        itemCount: users.length,
-        itemBuilder: (ctx, i) => UserTile(users.values.elementAt(i)),
+        itemCount: users.count,
+        itemBuilder: (ctx, i) => UserTile(users.byIndex(i)),
       ),
     );
   }
